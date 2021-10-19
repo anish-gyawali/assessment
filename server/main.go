fb65d90be9158693ffff380b9f1db62894186289
Anish Gyawali <anish.gyawali@selu.edu>
2021-10-19 14:42:00 -0500

    setup database connection with mysql

--- server/main.go
+++ server/main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
+	"github.com/anish-gyawali/assessment/server/config"
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+var (
+	db *gorm.DB = config.SetupDatabaseConnection()
 )
 
 func main() {
+	defer config.CloseDatabaseConnection(db)
 	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Initial set up",
-		})
-	})
 	r.Run()
 }
